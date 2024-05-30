372b35cab2bbed6d7a243a825658db775be4b219
Noelia <sharimanoe@gmail.com>
2024-05-30 15:45:37 +0200

    add Profile Styling

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -59,91 +59,140 @@ function ProfilePage() {
   if (!user) return <p>User data not found</p>;
 
   return (
-    <div className="user-detail">
-      <h1>User Detail</h1>
-      {editing ? (
-        <form onSubmit={handleSubmit} className="user-form">
-          <label>
-            Email:
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            Address:
-            <input
-              type="text"
-              name="address"
-              value={formData.address}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            Phone:
-            <input
-              type="text"
-              name="phone"
-              value={formData.phone}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            Bio:
-            <textarea
-              name="bio"
-              value={formData.bio}
-              onChange={handleChange}
-            ></textarea>
-          </label>
-          <label>
-            Avatar URL:
-            <input
-              type="text"
-              name="avatar"
-              value={formData.avatar}
-              onChange={handleChange}
-            />
-          </label>
-          <button type="submit">Save</button>
-          <button type="button" onClick={() => setEditing(false)}>
-            Cancel
-          </button>
-        </form>
-      ) : (
-        <div className="user-card">
-          <img
-            src={user.avatar || "/client/src/assets/defaultUser.png"}
-            alt={`${user.name}'s avatar`}
-            className="user-avatar"
-          />
-          <h2>{user.name}</h2>
-          <p>
-            <strong>Email:</strong> {user.email}
-          </p>
-          <p>
-            <strong>Address:</strong> {user.address}
-          </p>
-          <p>
-            <strong>Phone:</strong> {user.phone}
-          </p>
-          <p>
-            <strong>Bio:</strong> {user.bio}
-          </p>
-          <button onClick={() => setEditing(true)}>Edit</button>
+    <div class="bg-gray-100 dark:bg-gray-900">
+      <div class="w-full max-w-3xl mx-auto p-8">
+        <div class="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md border dark:border-gray-700">
+          <h1 class="text-2xl font-bold text-gray-800 dark:text-white mb-4">
+            Profile:
+          </h1>
+
+          <div class="mb-6">
+            <div class="mt-4">
+              <label
+                for="address"
+                class="block text-gray-700 dark:text-white mb-1"
+              >
+                First Name
+              </label>
+              <input
+                type="text"
+                id="first_name"
+                class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                disabled={!editing}
+              />
+            </div>
+            <div class="mt-4">
+              <label
+                for="address"
+                class="block text-gray-700 dark:text-white mb-1"
+              >
+                Mail
+              </label>
+              <input
+                type="text"
+                id="address"
+                class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                disabled={!editing}
+              />
+            </div>
+            <div class="mt-4">
+              <label
+                for="address"
+                class="block text-gray-700 dark:text-white mb-1"
+              >
+                Address
+              </label>
+              <input
+                type="text"
+                id="address"
+                class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none"
+                name="address"
+                value={formData.address}
+                onChange={handleChange}
+                disabled={!editing}
+              />
+            </div>
+
+            <div class="mt-4">
+              <label
+                for="city"
+                class="block text-gray-700 dark:text-white mb-1"
+              >
+                Phone
+              </label>
+              <input
+                type="text"
+                id="phone"
+                class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none"
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                disabled={!editing}
+              />
+            </div>
+
+            <div class="grid grid-cols-2 gap-4 mt-4">
+              <div>
+                <label
+                  for="state"
+                  class="block text-gray-700 dark:text-white mb-1"
+                >
+                  Bio
+                </label>
+                <input
+                  type="text"
+                  id="bio"
+                  class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none"
+                  name="bio"
+                  value={formData.bio}
+                  onChange={handleChange}
+                  disabled={!editing}
+                />
+              </div>
+              <div>
+                <label
+                  for="zip"
+                  class="block text-gray-700 dark:text-white mb-1"
+                >
+                  Avatar
+                </label>
+                <input
+                  type="text"
+                  id="avatar"
+                  class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none"
+                  name="avatar"
+                  value={formData.avatar}
+                  onChange={handleChange}
+                  disabled={!editing}
+                />
+              </div>
+            </div>
+          </div>
+          {editing ? (
+            <button
+              type="submit"
+              onClick={handleSubmit}
+              className="bg-teal-500 text-white px-4 py-2 rounded-lg hover:bg-teal-700 dark:bg-teal-600 dark:text-white dark:hover:bg-teal-900"
+            >
+              Save
+            </button>
+          ) : (
+            <button
+              type="button"
+              onClick={() => setEditing(true)}
+              className="bg-teal-500 text-white px-4 py-2 rounded-lg hover:bg-teal-700 dark:bg-teal-600 dark:text-white dark:hover:bg-teal-900"
+            >
+              Edit
+            </button>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 }
