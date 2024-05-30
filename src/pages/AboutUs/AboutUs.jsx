7480c82f86b81bdc0a328d479c63495020ec1c02
Noelia <sharimanoe@gmail.com>
2024-05-30 16:38:22 +0200

    style of about Us page

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,28 +1,44 @@
 import React from "react";
 import "./AboutUs.css";
+import foodTruck from "../../assets/Argentinian_Food_Truck.png";
 
 function AboutUs() {
   return (
-    <div className="about-us">
-      <h1>About Us</h1>
-      <p>
-        Welcome to <strong>BiFe</strong>, your go-to food truck for authentic
-        Argentinian cuisine! We pride ourselves on delivering the most delicious
-        and traditional dishes, from succulent steaks to mouth-watering
-        empanadas. Our mission is to bring the taste of Argentina to your
-        neighborhood with quality ingredients and a passion for cooking.
-      </p>
-      <p>
-        At <strong>BiFe</strong>, we believe in the power of good food to bring
-        people together. Our recipes are crafted with love and respect for our
-        culinary heritage, ensuring every bite transports you to the vibrant
-        streets of Argentina. Whether you're a meat lover or a fan of savory
-        pastries, our menu has something for everyone.
-      </p>
-      <p>
-        Thank you for choosing <strong>BiFe</strong>. We look forward to serving
-        you and sharing our love for Argentinian food with you!
-      </p>
+    <div class="sm:flex items-center max-w-screen-xl">
+      <div class="sm:w-1/2 p-10">
+        <div class="image object-center text-center">
+          <img src={foodTruck} />
+        </div>
+      </div>
+      <div class="sm:w-1/2 p-5">
+        <div class="text">
+          <span class="text-gray-500 border-b-2 border-indigo-600 uppercase">
+            About us
+          </span>
+          <h2 class="my-4 font-bold text-3xl  sm:text-4xl ">
+            About <span class="text-indigo-600">Our FoodTruck</span>
+          </h2>
+          <p class="text-gray-700">
+            Welcome to <strong>BiFe</strong>, your go-to food truck for
+            authentic Argentinian cuisine! We pride ourselves on delivering the
+            most delicious and traditional dishes, from succulent steaks to
+            mouth-watering empanadas. Our mission is to bring the taste of
+            Argentina to your neighborhood with quality ingredients and a
+            passion for cooking.
+          </p>
+          <p class="text-gray-700">
+            At <strong>BiFe</strong>, we believe in the power of good food to
+            bring people together. Our recipes are crafted with love and respect
+            for our culinary heritage, ensuring every bite transports you to the
+            vibrant streets of Argentina. Whether you're a meat lover or a fan
+            of savory pastries, our menu has something for everyone.
+          </p>
+          <p class="text-gray-700">
+            Thank you for choosing <strong>BiFe</strong>. We look forward to
+            serving you and sharing our love for Argentinian food with you!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
