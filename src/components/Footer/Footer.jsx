2029db806f8ed1e3424b49cda4919b0ea02c7c49
Noelia <sharimanoe@gmail.com>
2024-05-30 22:28:33 +0200

    style footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,42 +2,42 @@ import lgbtqFlag from "../../assets/lgbtq-flag.jpg";
 
 function Footer() {
   return (
-    <footer className="bg-black p-4 border-t-2 border-[#FFEDA3]">
-      <div className="flex flex-col md:flex-row justify-between text-center md:text-left">
-        <section className="mb-4">
-          <h2 className="slabo-27px-small text-[#FFEDA3]">Contact Us</h2>
-          <p className="slabo-27px-small text-[#FFEDA3]">
-            Email: info@bife.com
-          </p>
-          <p className="slabo-27px-small text-[#FFEDA3]">
-            Phone: (123) 456-7890
-          </p>
-          <p className="slabo-27px-small text-[#FFEDA3]">
-            Follow us on social media for updates on our location and specials!
-          </p>
-        </section>
-        <section className="mb-4">
-          <p className="slabo-27px-small text-[#FFEDA3]">
-            <a
-              href="https://github.com/sharimanoe/frontend-food-truck"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Visit our GitHub
-            </a>
-          </p>
-        </section>
-        <section className="mb-4">
-          <p className="slabo-27px-small text-[#FFEDA3]">
-            &copy; 2024 BiFe - All Rights Reserved
-          </p>
-        </section>
-        <section className="mt-4">
-          <p className="slabo-27px-small text-[#FFEDA3] flex items-center justify-center">
-            <img src={lgbtqFlag} alt="LGBTQ+ Flag" className="h-6 w-10 mr-2" />
-            LGBTQ+ Friendly
-          </p>
-        </section>
+    <footer class="bg-white shadow dark:bg-gray-900">
+      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+          <a
+            href="https://flowbite.com/"
+            class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+          >
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Bife
+            </span>
+          </a>
+          <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <li>
+              <a class="me-4 md:me-6">Email: info@bife.com</a>
+            </li>
+            <li>
+              <a class="me-4 md:me-6">Phone: (123) 456-7890</a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/sharimanoe/frontend-food-truck"
+                class="hover:underline me-4 md:me-6"
+              >
+                Visit our GitHub
+              </a>
+            </li>
+            <li className="flex items-center">
+              <img src={lgbtqFlag} alt="LGBTQ+ Flag" className="h-4 w-8 mr-2" />
+              <a class="me-4 md:me-6">LGBTQ+ Friendly</a>
+            </li>
+          </ul>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2024 <a>BiFe™</a>. All Rights Reserved.
+        </span>
       </div>
     </footer>
   );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -264,7 +264,7 @@ function HomePage() {
             <div className="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
               <img
                 className="hover:grow hover:shadow-lg"
-                src={logo}
+                src={product.image}
                 alt="product"
               />
               <div className="pt-3 flex items-center justify-between">
